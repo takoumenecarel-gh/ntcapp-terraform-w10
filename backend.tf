4f657fc9b7db262a06e48ce491b6a594134c648d
takoumenecarel-gh <takoumenecarel@icloud.com>
2025-09-30 20:48:12 -0600

    Add terraform CI/CD Pipeline

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket       = "w7-ct-terraform-bucket"
     key          = "week10/terraform.tfstate"
     region       = "us-east-1"
-    use_lockfile = false
+   // use_lockfile = false
   }
 }
