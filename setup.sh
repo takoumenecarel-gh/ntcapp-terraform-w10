427838c9f6e910463602b7cf695374c3bf2b3cc8
takoumenecarel-gh <takoumenecarel@icloud.com>
2025-09-30 20:44:53 -0600

    Add terraform CI/CD Pipeline

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ sudo systemctl start httpd
 sudo systemctl enable httpd
 echo "<html><h1><p> Welcome to Utrains.<br> You are redirected to ${HOSTNAME} to see how the load balancer is sharing the traffic.</p></h1></html>" | sudo tee /var/www/html/index.html 
 
+
